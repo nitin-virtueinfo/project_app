7e6420528dd570dcfa8210132f614a60499fbacf
nitin <nitin@nitin.(none)>
2014-01-02 13:12:41 +0530

    text area issue fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require bootstrap-datetimepicker
+//= require bootstrap-wysihtml5
 //= require turbolinks
 //= require_tree .
